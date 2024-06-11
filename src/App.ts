b0a0cdb0e0b68a26fd02e9bc3c4923bb2e684dd4
rvlwldev <102034841+rvlwldev@users.noreply.github.com>
2024-06-11 16:36:14 +0900

    feat: AppLoader로 동적 컨트롤러 맵핑 구현

--- src/App.ts
+++ src/App.ts
@@ -1,27 +1,29 @@
 import env from '@/configurations/Environment';
-import route from '@/configurations/App';
+import loader from '@/configurations/AppLoader';
 
-import express from 'express';
+import { Application } from 'express';
 import bodyParser from 'body-parser';
+import globalErrorHandler from '@/middlewares/ErrorHandler';
 
 import CUG from '@/databases/CUG';
 import CPG from '@/databases/CPG';
 import CCG from '@/databases/CCG';
 
-const app = express();
-app.use(route);
-app.use(bodyParser.json());
+loader().then(async (app: Application) => {
+	app.use(bodyParser.json());
+	app.use(globalErrorHandler);
 
-const port = env.PORT || 3000;
-app.listen(port, async () => {
-	await CUG.connect();
-	await CPG.connect();
-	await CCG.connect();
+	const port = env.PORT || 3000;
+	app.listen(port, async () => {
+		await CUG.connect();
+		await CPG.connect();
+		await CCG.connect();
 
-	console.info(`\nMode : ${process.env.NODE_ENV}`);
-	console.info(`
+		console.info(`\nMode : ${process.env.NODE_ENV}`);
+		console.info(`
     ######################################
         Server is running on port ${port}
     ######################################
     `);
+	});
 });
