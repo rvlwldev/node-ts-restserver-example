677061c4b2b95ec35e72941ac9c868a346d5c591
rvlwldev <102034841+rvlwldev@users.noreply.github.com>
2024-06-17 10:02:25 +0900

    chore: 타입 경로 수정 및 index 생성

---
+++ src/domains/_common/types/index.ts
@@ -0,0 +1,5 @@
+import { SelectResult, InsertResult, UpdateResult, DeleteResult } from './Types';
+
+import { User } from './User';
+
+export { User, SelectResult, InsertResult, UpdateResult, DeleteResult };
---
+++ src/domains/cpg/Types.d.ts
@@ -0,0 +1,27 @@
+import { User } from '@common/types';
+
+interface User extends User {
+	sex: string;
+	groups: string;
+	area: string;
+	farm_name: string;
+	main_goods: string;
+	phone1: string;
+	phone2: string;
+	fax: string;
+	zip_code: string;
+	addr1: string;
+	addr2: string;
+	login: string;
+	login_num: number;
+	admin: string;
+	del: string;
+	homepage: string;
+	apc: string;
+	m_code: string;
+	mf_code1: string;
+	mf_code2: string;
+	o_codelist: string;
+	apc_man: string;
+	apc_list: string;
+}
